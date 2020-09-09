b667385097d9337c61440918607026965d3a0ab9
JIA YEE <jiayeerawr@gmail.com>
2020-09-10 00:17:38 +0800

    feat: add simple and compound interest calculators

---
+++ calcsg-calc/index.js
@@ -0,0 +1,7 @@
+const { calcCompoundInterest } = require('./calc/compound-interest');
+const { calcSimpleInterest } = require('./calc/simple-interest');
+
+module.exports = {
+  calcSimpleInterest,
+  calcCompoundInterest,
+};
