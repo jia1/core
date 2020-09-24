fba5b3a1fcabccc4f7538737f5adfbe167bf37ed
JIA YEE <jiayeerawr@gmail.com>
2020-09-24 22:25:29 +0800

    refactor: move calculators to src/library/calc and use nerdamer to solve simple and compound interest equations

--- calcsg-calc/index.js
+++
@@ -1,7 +0,0 @@
-const { calcCompoundInterest } = require('./calc/compound-interest');
-const { calcSimpleInterest } = require('./calc/simple-interest');
-
-module.exports = {
-  calcSimpleInterest,
-  calcCompoundInterest,
-};
---
+++ src/library/calc/interest.js
@@ -0,0 +1,60 @@
+import nerdamer from "nerdamer/nerdamer.core";
+import Algebra from "nerdamer/Algebra";
+import Calculus from "nerdamer/Calculus";
+import Solve from "nerdamer/Solve";
+
+export const calcCompoundInterest = ({
+  principal,
+  interestRate,
+  compoundRate = 1,
+  numInterestDeposits,
+}) => {
+  const totalAmount = solveForOneUnknownVariable(
+    "a=p*(1+(r/n))^(n*t)",
+    {
+      p: principal.toString(),
+      r: interestRate.toString(),
+      n: compoundRate.toString(),
+      t: numInterestDeposits.toString(),
+    },
+    "a",
+    principal
+  );
+  return {
+    totalAmount,
+    totalInterest: totalAmount - principal,
+  };
+};
+
+export const calcSimpleInterest = ({
+  principal,
+  interestRate,
+  numInterestDeposits,
+}) => {
+  const totalAmount = solveForOneUnknownVariable(
+    "a=p+p*r*t",
+    {
+      p: principal.toString(),
+      r: interestRate.toString(),
+      t: numInterestDeposits.toString(),
+    },
+    "a",
+    principal
+  );
+  return {
+    totalAmount,
+    totalInterest: totalAmount - principal,
+  };
+};
+
+const solveForOneUnknownVariable = (
+  equationString,
+  knownValues,
+  unknownVariable,
+  defaultValue
+) => {
+  const equation = nerdamer(equationString).evaluate(knownValues);
+  const solution = equation.solveFor(unknownVariable);
+  const answer = solution.toDecimal();
+  return answer;
+};
