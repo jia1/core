2a364d7cd9bacf250d053b1ff4f2ffcc20c08778
JIA YEE <jiayeerawr@gmail.com>
2020-10-04 21:37:46 +0800

    feat: extend compound interest function to accept and process periodic deposits

--- src/library/calc/__tests__/interest.test.js
+++ src/library/calc/__tests__/interest.test.js
@@ -15,15 +15,17 @@ test('calculates simple interest', () => {
 
 test('calculates compound interest', () => {
   const principal = 10000;
-  const interestRatePerAnnum = 0.025;
-  const compoundRatePerAnnum = 12;
+  const depositAmountPerMonth = 500;
+  const interestRatePerAnnum = 0.01;
+  const compoundRatePerMonth = 1;
   const totalMonths = 36;
   const { totalAmount, totalInterest } = calcCompoundInterest({
     principal,
+    depositAmountPerMonth,
     interestRatePerAnnum,
-    compoundRatePerAnnum,
+    compoundRatePerMonth,
     totalMonths,
   });
-  expect(Number(totalAmount.toFixed(2))).toBe(10778.77);
-  expect(Number(totalInterest.toFixed(2))).toBe(778.77);
+  expect(Number(totalAmount.toFixed(2))).toBe(28569.41);
+  expect(Number(totalInterest.toFixed(2))).toBe(569.41);
 });
--- src/library/calc/interest.js
+++ src/library/calc/interest.js
@@ -7,16 +7,18 @@ const totalMonthsPerYear = 12;
 
 export const calcCompoundInterest = ({
   principal,
+  depositAmountPerMonth = 0,
   interestRatePerAnnum,
-  compoundRatePerAnnum = 1,
+  compoundRatePerMonth = 1,
   totalMonths,
 }) => {
   const totalAmount = solveForOneUnknownVariable(
-    'a=p*(1+(r/n))^(n*t)',
+    'a=(p*(1+(r/n))^(n*t))+(q*(((1+(r/n))^(n*t)-1)/(r/n)))',
     {
       p: principal.toString(),
+      q: depositAmountPerMonth.toString(),
       r: (interestRatePerAnnum / totalMonthsPerYear).toString(),
-      n: compoundRatePerAnnum.toString(),
+      n: compoundRatePerMonth.toString(),
       t: totalMonths.toString(),
     },
     'a',
@@ -24,7 +26,8 @@ export const calcCompoundInterest = ({
   );
   return {
     totalAmount,
-    totalInterest: totalAmount - principal,
+    totalInterest:
+      totalAmount - (principal + depositAmountPerMonth * totalMonths),
   };
 };
 
